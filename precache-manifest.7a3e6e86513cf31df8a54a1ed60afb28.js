e1cdfc9999fb1e7a0d9bac66b893a04e5f6c5451
Kateryna Kurochka <kateryna.kurochka@teladoc.com>
2022-07-11 21:04:33 +0300

    Updates

---
+++ precache-manifest.7a3e6e86513cf31df8a54a1ed60afb28.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93ff49eceaa2905408cc6ff64264aa8e",
+    "url": "/music-app-test-task/index.html"
+  },
+  {
+    "revision": "f674f12b27f85bafd006",
+    "url": "/music-app-test-task/static/css/main.2843a3b0.chunk.css"
+  },
+  {
+    "revision": "d557b6fa98c0f1d97bb5",
+    "url": "/music-app-test-task/static/js/2.0375a179.chunk.js"
+  },
+  {
+    "revision": "2d0dc3ea56ea29aa89323d664321ba6d",
+    "url": "/music-app-test-task/static/js/2.0375a179.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f674f12b27f85bafd006",
+    "url": "/music-app-test-task/static/js/main.888617ca.chunk.js"
+  },
+  {
+    "revision": "ff9d56071e9f69f61e94",
+    "url": "/music-app-test-task/static/js/runtime-main.00b94735.js"
+  }
+]);
